e92ecc23226cc46ec69c7d01a81e288d20a7360f
ALNAK <aleenn@ocadu.ca>
2025-02-02 23:59:04 -0500

    VersionTwo

---
+++ script.js
@@ -0,0 +1,44 @@
+0; // comments uing this :>
+134151855;
+12341.1515;
+Infinity;
+
+var kitty; //declared a variable for store space in memory
+
+kitty = 5; // definition with a data type of number with the number of 5
+
+var Kitty = 24; // semi are end of statement, this line has a definition with a data type number with the number of 24
+
+console.log(kitty);
+
+kitty = Kitty; // = assignment operator left to right
+
+console.log(kitty);
+
+var doggy; // declared a variable but not defined a value
+console.log(doggy); // undefined
+
+// numbers , undefined, strings
+
+var prose = 'alksdjfas alksjsdlkfj asdflakjsd kda "lskdjflksd" '; // is a string delimiter
+var moreprose = "I said 'something' inside of your string";
+
+// booleans are true and false
+
+var x = true;
+x = false;
+
+// objects
+var ball = {}; // empty object
+ball = null; // null is a datatype in Javascript
+
+// operators
+5 + 5;
+kitty = 5 + 5;
+var kitty; // becomes undefined because we are now referring to a new place
+
+kitty = 5 + "5"; // string concatenation operator
+console.log(kitty);
+
+console.log(kitty + "lskdjflskjdflkj");
+
